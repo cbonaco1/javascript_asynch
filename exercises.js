fecee134538a389e9d0b7f5e3f25031d7db10dc6
ChristianJules <ChristianJules@aa.io>
2016-01-08 11:47:29 -0500

    Create clock.

--- exercises.js
+++ exercises.js
@@ -1,22 +1,33 @@
-function absurdTimes(numTimes, fun) {
-  var i = 0;
-
-  function loopStep() {
-    if (i == numTimes) {
-      // we're done, stop looping
-      return;
-    }
-
-    fun();
-
-    // recursively call loopStep
-    i += 1;
-    loopStep();
-  }
-
-  loopStep();
+function Clock() {
+  // assume that the callback we pass will be called function style
+  // and therefore 'this' will be window. therefore you need to pass 'this' with
+  // .bind
+  setInterval(this._tick.bind(this), 1000);
 }
 
-absurdTimes(5, function(){
-  console.log("Enter absurdTimes");
-});
+// function setInterval (callback) {
+//   // ...
+//     callback(); // this === window
+// }
+//
+// function mySetInterval (callback) {
+//   // ...
+//     cat.callback();
+//   // if you do this, you would not need .bind(this)
+// }
+
+Clock.prototype.printTime = function () {
+  var time = "" + this.hours + ":" + this.minutes + ":" + this.seconds + "";
+  console.log("Current Time: " + time);
+};
+
+Clock.prototype._tick = function () {
+  var time_now = new Date();
+  this.hours = time_now.getHours();
+  this.minutes = time_now.getMinutes();
+  this.seconds = time_now.getSeconds();
+
+  this.printTime();
+};
+
+var clock = new Clock();
